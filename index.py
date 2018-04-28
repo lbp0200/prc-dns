e09faa5b37f5f1a72faf21eb5a330ebb0ecf8ec7
lbp0200 <lbp@liuboping.com>
2018-04-28 18:41:16 +0800

    add log level

--- index.py
+++ index.py
@@ -5,6 +5,19 @@ import socket
 import threading
 import SocketServer
 import logging
+import argparse
+from enum import Enum
+
+
+class LogLevel(Enum):
+    debug = 'DEBUG'
+    info = 'INFO'
+    warning = 'WARNING'
+    error = 'ERROR'
+    critical = 'CRITICAL'
+
+    def __str__(self):
+        return self.value
 
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
@@ -24,6 +37,19 @@ class ThreadedUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
     pass
 
 
+def get_arg():
+    """解析参数"""
+    parser = argparse.ArgumentParser(prog='prc-dns', description='google dns proxy.')
+    parser.add_argument('-v', '--verbose', help='log out DEBUG', action="store_true")
+    parser.add_argument('--log', help='Log Level,default ERROR', type=LogLevel, choices=list(LogLevel), default='ERROR')
+    parser.add_argument('-l', '--listen', help='listening IP,default 0.0.0.0', default='0.0.0.0')
+    parser.add_argument('-p', '--port', help='listening Port,default 3535', default=3535)
+    parser.add_argument('-r', '--proxy', help='Used For Query Google DNS,default direct', default=None)
+    parser.add_argument('-ut', '--tcp_udp', help='DNS protocol, tcp udp or both', default='udp')
+
+    return parser.parse_args()
+
+
 def client(ip, port, message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
@@ -35,7 +61,16 @@ def client(ip, port, message):
         sock.close()
 
 
-if __name__ == "__main__":
+def main():
+    args = get_arg()
+    if args.verbose:
+        args.log = 'DEBUG'
+    loglevel = args.log
+    numeric_level = getattr(logging, str(loglevel).upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+    logging.basicConfig(format='%(asctime)s %(message)s', level=numeric_level)
+    
     # Port 0 means to select an arbitrary unused port
     HOST, PORT = "localhost", 0
 
@@ -56,3 +91,7 @@ if __name__ == "__main__":
 
     tcp_server.shutdown()
     tcp_server.server_close()
+
+
+if __name__ == "__main__":
+    main()
