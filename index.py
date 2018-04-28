1923ab0cfa69cfe6f83d27d86ac7f53355a2d0b6
lbp0200 <lbp@liuboping.com>
2018-04-28 17:50:57 +0800

    init

--- index.py
+++ index.py
@@ -4,6 +4,7 @@
 import socket
 import threading
 import SocketServer
+import logging
 
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
@@ -19,13 +20,17 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
 
+class ThreadedUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer):
+    pass
+
+
 def client(ip, port, message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
     try:
         sock.sendall(message)
         response = sock.recv(1024)
-        print("Received: {}".format(response))
+        logging.info("Received: {}".format(response))
     finally:
         sock.close()
 
@@ -34,20 +39,20 @@ if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
     HOST, PORT = "localhost", 0
 
-    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    ip, port = server.server_address
+    tcp_server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    ip, port = tcp_server.server_address
 
     # Start a thread with the server -- that thread will then start one
     # more thread for each request
-    server_thread = threading.Thread(target=server.serve_forever)
+    tcp_server_thread = threading.Thread(target=tcp_server.serve_forever)
     # Exit the server thread when the main thread terminates
-    server_thread.daemon = True
-    server_thread.start()
-    print("Server loop running in thread:", server_thread.name)
+    tcp_server_thread.daemon = True
+    tcp_server_thread.start()
+    logging.info("Server loop running in thread:", tcp_server_thread.name)
 
     client(ip, port, "Hello World 1")
     client(ip, port, "Hello World 2")
     client(ip, port, "Hello World 3")
 
-    server.shutdown()
-    server.server_close()
+    tcp_server.shutdown()
+    tcp_server.server_close()
