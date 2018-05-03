065ecc853be3cc5c9743a6b2a6b818b52246ae34
lbp0200 <lbp@liuboping.com>
2018-05-03 13:56:28 +0800

    add log level

--- php/dns.php
+++ php/dns.php
@@ -1,11 +1,18 @@
 <?php
+$is_https = false;
+if (isset($_SERVER['HTTPS'])) {
+    $is_https = true;
+}
 if (count($_GET) === 0) {
     exit();
 }
 $root = 'https://dns.google.com/resolve?';
 $parameters = [];
 foreach ($_GET as $k => $v) {
-    $parameters[] = "{$k}={$v}";
+    $dv = $is_https ? $v : base64_decode(urldecode($v));
+    $parameters[] = "{$k}={$dv}";
 }
 $url = $root . (implode('&', $parameters));
-echo file_get_contents($url);
+
+$resp = file_get_contents($url);
+echo $is_https ? $resp : base64_encode($resp);
